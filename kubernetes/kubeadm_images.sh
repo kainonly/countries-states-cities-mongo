866ba4058598edc45a78b73fbd7c2d91780d127a
kain <zhangtqx@vip.qq.com>
2019-09-09 18:07:49 +0800

    update

--- kubernetes/kubeadm_images.sh
+++ kubernetes/kubeadm_images.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+
 declare -A images=(
     ['k8s.gcr.io/pause:3.1']='kainonly/pause:3.1'
     ['k8s.gcr.io/etcd:3.3.10']='kainonly/etcd:3.3.10'
     ['k8s.gcr.io/coredns:1.3.1']='kainonly/coredns:1.3.1'
-    ['k8s.gcr.io/kube-apiserver:v1.15.3']='kainonly/kube-apiserver:v1.15.3',
-    ['k8s.gcr.io/kube-controller-manager:v1.15.3']='kainonly/kube-controller-manager:v1.15.3',
+    ['k8s.gcr.io/kube-apiserver:v1.15.3']='kainonly/kube-apiserver:v1.15.3'
+    ['k8s.gcr.io/kube-controller-manager:v1.15.3']='kainonly/kube-controller-manager:v1.15.3'
     ['k8s.gcr.io/kube-proxy:v1.15.3']='kainonly/kube-proxy:v1.15.3'
     ['k8s.gcr.io/kube-scheduler:v1.15.3']='kainonly/kube-scheduler:v1.15.3'
 )
