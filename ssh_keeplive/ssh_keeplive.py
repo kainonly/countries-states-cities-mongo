24b70c5134ec66e638cbc532faeb4a7b419fc08f
kain <zhangtqx@vip.qq.com>
2020-01-07 10:10:21 +0800

    更新ssh_keeplive

--- ssh_keeplive/ssh_keeplive.py
+++ ssh_keeplive/ssh_keeplive.py
@@ -3,6 +3,7 @@ import sys
 from os import path
 import re
 import subprocess
+import string
 
 if len(sys.argv) < 3:
     exit("Please add operating parameters!")
@@ -17,17 +18,33 @@ try:
     with open(input_path, mode='r') as f:
         content = f.read()
 
-    content = re.sub('\n(|\W+)#(|\W+)ClientAliveInterval\W+[0-9]+', '', content)
-    content = re.sub('\n(|\W+)#(|\W+)ClientAliveCountMax\W+[0-9]+', '', content)
-    content += '''
-    ClientAliveInterval 15
-    ClientAliveCountMax 45
-    '''
+    raws = []
+    exists_interval = False
+    exists_count_max = False
+    for line_text in content.split('\n'):
+        if re.search('(|\W+)#(|\W+)ClientAliveInterval\W+[0-9]+', line_text):
+            continue
+        if re.search('(|\W+)ClientAliveInterval\W+[0-9]+', line_text):
+            raws.append("ClientAliveInterval 15")
+            exists_interval = True
+            continue
+        if re.search('(|\W+)#(|\W+)ClientAliveCountMax\W+[0-9]+', line_text):
+            continue
+        if re.search('(|\W+)ClientAliveCountMax\W+[0-9]+', line_text):
+            raws.append("ClientAliveCountMax 45")
+            exists_count_max = True
+            continue
+        raws.append(line_text)
+
+    if exists_interval == False:
+        raws.append("ClientAliveInterval 15")
+    if exists_count_max == False:
+        raws.append("ClientAliveCountMax 45")
 
     with open(output_path, mode='w') as f:
-        f.write(content)
+        f.write('\n'.join(raws))
 
-    subprocess.run(["systemctl", "restart", "sshd"])
+    subprocess.run(['systemctl', 'restart', 'sshd'])
     print('ssh_keeplive keeplive set successfully!')
 except Exception as e:
     print(e)
