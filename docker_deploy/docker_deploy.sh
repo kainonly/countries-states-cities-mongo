9571ff0015b21d4cfc36b1c08c39e47f15390333
kain <zhangtqx@vip.qq.com>
2020-01-07 10:44:47 +0800

    update:docker_deploy

--- docker_deploy/docker_deploy.sh
+++ docker_deploy/docker_deploy.sh
@@ -32,4 +32,11 @@ sudo apt update
 echo "Install the latest version of Docker Engine"
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 
+echo "Enable Docker Service"
 sudo systemctl enable docker
+
+echo "Run this command to download the current stable release of Docker Compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+echo "Apply executable permissions to the binary"
+sudo chmod +x /usr/local/bin/docker-compose
