00c3f2e3e8d08ad5a4638216440f5843cf0861be
kain <kainpro@foxmail.com>
2020-07-22 17:19:42 +0800

    update

---
+++ change_ssl_for_qcloud/test.py
@@ -0,0 +1,34 @@
+import unittest
+import os
+from change_ssl_for_qcloud import ChangeSSL
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.deploy = ChangeSSL(os.environ['qcloud_id'], os.environ['qcloud_key'])
+
+    def test_search(self):
+        response = self.deploy.search('*.kainonly.com')
+        self.assertIsNotNone(response)
+        print(response.TotalCount)
+        print(response.Certificates)
+
+    def test_delete(self):
+        response = self.deploy.search('*.kainonly.com')
+        self.assertIsNotNone(response)
+        if response.TotalCount != 0:
+            for v in response.Certificates:
+                response = self.deploy.delete(v.CertificateId)
+                self.assertIsNotNone(response)
+
+    def test_upload(self):
+        with open('../resource/fullchain.pem') as f:
+            pub_key = f.read()
+        with open('../resource/privkey.pem') as f:
+            pri_key = f.read()
+        response = self.deploy.upload(pub_key, pri_key)
+        self.assertIsNotNone(response)
+
+
+if __name__ == '__main__':
+    unittest.main()
