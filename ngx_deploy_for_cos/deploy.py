d7080ebc8191bfa9cdc65e347fa794cbacbed127
kain <kainpro@foxmail.com>
2020-07-22 15:04:59 +0800

    update

--- ngx_deploy_for_cos/deploy.py
+++ ngx_deploy_for_cos/deploy.py
@@ -3,51 +3,51 @@ import json
 from qcloud_cos import CosConfig, CosS3Client
 
 with open('package.json') as f:
-  package = json.loads(f.read())
+    package = json.loads(f.read())
 
 Project = package['name']
 Deploy = package['deploy']['prod']
 
 config = CosConfig(
-  Region=Deploy['region'],
-  SecretId=Deploy['secretId'],
-  SecretKey=Deploy['secretKey']
+    Region=Deploy['region'],
+    SecretId=Deploy['secretId'],
+    SecretKey=Deploy['secretKey']
 )
 client = CosS3Client(config)
 
 marker = ""
 while True:
-  response = client.list_objects(
-    Bucket=Deploy['bucket'],
-    Marker=marker
-  )
-
-  if 'Contents' in response.keys():
-    objects = map(lambda v: {'Key': v['Key']}, response['Contents'])
-    client.delete_objects(
-      Bucket=Deploy['bucket'],
-      Delete={
-        'Object': list(objects),
-        'Quiet': 'true'
-      }
+    response = client.list_objects(
+        Bucket=Deploy['bucket'],
+        Marker=marker
     )
 
-  if response['IsTruncated'] == 'false':
-    break
+    if 'Contents' in response.keys():
+        objects = map(lambda v: {'Key': v['Key']}, response['Contents'])
+        client.delete_objects(
+            Bucket=Deploy['bucket'],
+            Delete={
+                'Object': list(objects),
+                'Quiet': 'true'
+            }
+        )
 
-  marker = response['NextMarker']
+    if response['IsTruncated'] == 'false':
+        break
+
+    marker = response['NextMarker']
 
 print('Clear COS OK!')
 
 for root, dir, files in os.walk('dist/' + Project):
-  for file in files:
-    local = os.path.join(root, file).replace('\\', '/')
-    key = local.replace('dist/' + Project + '/', '')
-    client.upload_file(
-      Bucket=Deploy['bucket'],
-      LocalFilePath=local,
-      Key=key
-    )
-    print('Send <' + key + '> Success')
+    for file in files:
+        local = os.path.join(root, file).replace('\\', '/')
+        key = local.replace('dist/' + Project + '/', '')
+        client.upload_file(
+            Bucket=Deploy['bucket'],
+            LocalFilePath=local,
+            Key=key
+        )
+        print('Send <' + key + '> Success')
 
 print('Sync COS OK!')
--- ngx_deploy_for_oss/deploy.py
+++ ngx_deploy_for_oss/deploy.py
@@ -3,7 +3,7 @@ import json
 from oss2 import Auth, Bucket, ObjectIterator
 
 with open('package.json') as f:
-  package = json.loads(f.read())
+    package = json.loads(f.read())
 
 Project = package['name']
 Deploy = package['deploy']['prod']
@@ -12,16 +12,16 @@ auth = Auth(Deploy['accessKeyId'], Deploy['accessKeySecret'])
 bucket = Bucket(auth, Deploy['endpoint'], Deploy['bucket'])
 
 for obj in ObjectIterator(bucket):
-  bucket.delete_object(obj.key)
-  print('Delete <' + obj.key + '> Success')
+    bucket.delete_object(obj.key)
+    print('Delete <' + obj.key + '> Success')
 
 print('Clear OSS OK!')
 
-for root, dir, files in os.walk('dist/' + Project):
-  for file in files:
-    local = os.path.join(root, file).replace('\\', '/')
-    key = local.replace('dist/' + Project + '/', '')
-    bucket.put_object_from_file(key, local)
-    print('Send <' + key + '> Success')
+for root, _, files in os.walk('dist/' + Project):
+    for file in files:
+        local = os.path.join(root, file).replace('\\', '/')
+        key = local.replace('dist/' + Project + '/', '')
+        bucket.put_object_from_file(key, local)
+        print('Send <' + key + '> Success')
 
 print('Sync OSS OK!')
