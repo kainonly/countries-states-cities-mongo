37ea5e7878e81fbad6f11802dc2ec41a865fea5f
kain <kainpro@foxmail.com>
2022-03-02 16:22:04 +0800

    更新

--- app/common.go
+++ app/common.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"countries-states-cities-mongo/common"
+	"github.com/go-resty/resty/v2"
 	"github.com/google/wire"
 )
 
@@ -10,5 +11,7 @@ var Provides = wire.NewSet(New)
 func New(i *common.Inject) *App {
 	return &App{
 		Inject: i,
+		Client: resty.New().
+			SetBaseURL(`https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master`),
 	}
 }
